88bece4a50d11846acc43b0382c73f1a1edbb51e
Deyvid0104 <driotol124@g.educaand.es>
2025-03-18 11:53:19 +0100

    sd

--- src/pokeWeb/Footer.jsx
+++ src/pokeWeb/Footer.jsx
@@ -4,7 +4,7 @@ import styles from './styles/footer.module.css';
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>Deyvid Rios - ASIR  2025</p>
+      <p>Deyvid Rios - ASIR 2025</p>
     </footer>
   );
 }
