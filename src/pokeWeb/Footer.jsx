bf5bded6114d40f49c6f7a4bda9cafa5fc76b25e
Deyvid0104 <driotol124@g.educaand.es>
2025-03-18 14:10:58 +0100

    pry

--- src/pokeWeb/Footer.jsx
+++ src/pokeWeb/Footer.jsx
@@ -4,7 +4,7 @@ import styles from './styles/footer.module.css';
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>Deyvid Rios - 2025</p>
+      <p>Deyvid Rios - IAW 2025</p>
     </footer>
   );
 }
